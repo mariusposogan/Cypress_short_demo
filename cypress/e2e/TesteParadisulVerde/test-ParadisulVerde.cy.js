04f5fbe7f55ce328c1615df258675c4d9c903ee5
Marius Posogan <35148052+mariusposogan@users.noreply.github.com>
2024-03-24 15:59:01 +0200

    Update test-ParadisulVerde.cy.js

--- cypress/e2e/TesteParadisulVerde/test-ParadisulVerde.cy.js
+++ cypress/e2e/TesteParadisulVerde/test-ParadisulVerde.cy.js
@@ -1,38 +1,33 @@
-describe("Site RescueTime", () => {
+describe("ParadisulVerde", () => {
   it("I can login", () => {
     cy.visit("https://www.paradisulverde.com");
-
     cy.get(
       "#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowallSelection"
     ).click();
     cy.get('[data-widget_type="sign-in.default"]').eq(1).click();
     cy.get("#field-email").type("mcposogan@gmail.com");
-    cy.get("#field-password").type("parola=123");
+    cy.get("#field-password").type("secret<123");
     cy.get("#submit-login").click();
-
-    cy.get("#history-link > .link-item > .material-icons").should("exist"); // verifica un element pe pagina
-    cy.screenshot(); // Face screenshot la pagina intr-un folder default
+    cy.get("#history-link > .link-item > .material-icons").should("exist"); 
+    cy.screenshot(); 
   });
 
   it("I can logout", () => {
     cy.visit("https://www.paradisulverde.com");
-
     cy.get(
       "#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowallSelection"
     ).click();
     cy.get('[data-widget_type="sign-in.default"]').eq(1).click();
-
     cy.get("#field-email").type("mcposogan@gmail.com");
-    cy.get("#field-password").type("parola=123");
+    cy.get("#field-password").type("secret<123");
     cy.get("#submit-login").click();
+    cy.get("#history-link > .link-item > .material-icons").should("exist"); 
 
-    cy.get("#history-link > .link-item > .material-icons").should("exist"); // verifica un element pe pagina
+    cy.wait(4000);
 
-    cy.wait(4000); // asteapta 4 sec
+    cy.get(".text-sm-center > a").click();
 
-    cy.get(".text-sm-center > a").click(); // click pe deconectare
-
-    cy.url().should("include", "back=my-account"); // verifica un segment de url
+    cy.url().should("include", "back=my-account"); 
     
   });
 });
